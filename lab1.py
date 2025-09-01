ac046858fb88dcae0aaeca6bd7957620b8de42b6
aumdave1979 <aumdave1979@gmail.com>
2025-09-01 09:49:33 +0530

    First post lab containing all 3 question of lab

---
+++ lab1.py
@@ -0,0 +1,19 @@
+# Write a program that displays “Welcome to Python” five times.
+
+def a():
+    for i in range(5):
+        print("Welcome to Python")
+
+# Write a program that displays the following table:
+
+def b():
+    print("a   a^2   a^3")
+    for a in range(1, 5):
+     print(f"{a:<3}{a**2:<6}{a**3}")
+
+#Write a program that displays the result of
+
+def c():
+   result = (9.5*4.5 - 2.5*3) / (45.5 - 3.5)
+   print("The result is:",result)
+   
